c904b67ee8b66abaf2662258aa0bce1c0f8bdb6a
Moses <asugam9@gmail.com>
2024-11-19 22:31:15 +0300

    MERN

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+// const express = require('express');
+import express from 'express';
+
+const app = express();
+
+app.get("/product", (req, res) =>{
+    
+});
+
+app.listen(5000,()=>{
+    console.log("Server has started at hhtp://localhost:5000")
+});
